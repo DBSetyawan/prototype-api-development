d1e3bb6cb028f4313c71b770233babde5c7d960e
Daniel <artexsdns@gmail.com>
2020-06-10 16:03:07 +0700

    OpenAPI publish

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+// class Controller
+// {
+    /**
+     * @OA\Info(
+     *     description="3PS API Development",
+     *     version="1.0.0",
+     *     title="3PS Application api development",
+     *     termsOfService="http://swagger.io/terms/",
+     *     @OA\Contact(
+     *         email="admin@admin.com"
+     *     ),
+     *     @OA\License(
+     *         name="Apache 2.0",
+     *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *     )
+     * )
+     */
+
+
+  
+    class Controller extends BaseController
+    {
+        use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    }
+// }
---
+++ app/Http/Kernel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \App\Http\Middleware\TrimStrings::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    protected $middlewareGroups = [
+        'api' => [
+            'throttle:60,1',
+            'bindings',
+            \App\Http\Middleware\AuthGates::class,
+        ],
+        'client_credentials' => [
+            CheckClientCredentials::class,
+            'throttle:60,1',
+            'bindings',
+        ],
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\AuthGates::class,
+            \App\Http\Middleware\SetLocale::class,
+        ],
+    ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'client' => CheckClientCredentials::class,
+    ];
+}
