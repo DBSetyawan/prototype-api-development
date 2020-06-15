8936ccd8cabc62508d30bd419a9cc2c3de62852b
Daniel <artexsdns@gmail.com>
2020-06-15 14:29:53 +0700

    change permission

--- app/Http/Requests/MassDestroyProjectRequest.php
+++ app/Http/Requests/MassDestroyProjectRequest.php
@@ -11,7 +11,7 @@ class MassDestroyProjectRequest extends FormRequest
 {
     public function authorize()
     {
-        abort_if(Gate::denies('project_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        // abort_if(Gate::denies('project_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return true;
     }
